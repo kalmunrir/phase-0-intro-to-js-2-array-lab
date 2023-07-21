48aa99af681dc3e68d7b9fcdc2c5fe6a2f1f63a1
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-07-20 20:11:50 -0600

    Completed lab

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ function destructivelyAppendCat(name) {
 function destructivelyPrependCat(name) {
     cats.unshift(name);
 }
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return [...cats].splice(1);
+}
